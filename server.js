f5bae3164333a73f8ea87917ed589f2ba2b063b8
AsadProgrammer786 <102869276+AsadProgrammer786@users.noreply.github.com>
2022-04-05 16:22:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+// const http = require('http');
 const port = process.env.PORT || 80;
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.end("hi");
 });
-server.listen(port, () => {
+app.listen(port, () => {
     console.log('listening on *:3000');
 });
-io.on('connection', socket => {
-    console.log("hihi");
-    socket.on('message', (ar) => {
-        console.log("done");
-    })
-});
+// io.on('connection', socket => {
+//     console.log("hihi");
+//     socket.on('message', (ar) => {
+//         console.log("done");
+//     })
+// });
